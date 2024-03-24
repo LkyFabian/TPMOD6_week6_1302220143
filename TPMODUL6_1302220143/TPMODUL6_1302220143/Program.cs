07b790d0a46564881acea147ca723b1537a6e01d
Jeremy Fabian <118284208+LkyFabian@users.noreply.github.com>
2024-03-25 01:12:56 +0700

    Hasil yang sebelumnya

---
+++ TPMODUL6_1302220143/TPMODUL6_1302220143/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SayaTubeVideo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Jeremy Mathew Fabian Sitepu");
+
+            Console.WriteLine("Initial video details:");
+            video.PrintVideoDetails();
+
+            Console.WriteLine("\nIncreasing play count...");
+
+            for (int i = 0; i < 100; i++)
+            {
+                video.IncreasePlayCount(10000001);
+            }
+
+            Console.WriteLine("\nVideo details after increasing play count:");
+            video.PrintVideoDetails();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TPMODUL6_1302220143/TPMODUL6_1302220143/SayaTubeVideo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace SayaTubeVideo
+{
+    public class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public const int maxTitleLength = 100;
+        public const int maxPlayCountIncreasePerCall = 10000000;
+
+        public SayaTubeVideo(string title)
+        {
+            if (string.IsNullOrWhiteSpace(title) || title.Length > maxTitleLength)
+                throw new ArgumentException("Title cannot be null, empty, or longer than " + maxTitleLength + " characters.");
+
+            this.id = GenerateRandomId();
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            if (count > maxPlayCountIncreasePerCall)
+                throw new ArgumentOutOfRangeException("count", "Must not exceed " + maxPlayCountIncreasePerCall);
+
+            try
+            {
+                checked
+                {
+                    this.playCount += count;
+                }
+            }
+            catch (OverflowException ex)
+            {
+                throw new InvalidOperationException("IncreasePlayCount: Operation generated arithmetic overflow exception", ex);
+            }
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Video Details: ID=" + id + ", Title=" + title + ", Play Count=" + playCount);
+        }
+
+        private int GenerateRandomId()
+        {
+            Random rand = new Random();
+            return rand.Next(10000, 100000);
+        }
+
+        public int Id { get => id; }
+        public string Title { get => title; }
+        public int PlayCount { get => playCount; }
+    }
+}
